b47f225bc651fac6cb3119d0683b618f57a6fdef
“Rodrigo <rodrigo_piranguinho@hotmail.com>
2025-01-02 15:59:02 -0300

    feat(deploy): deploy 2025-02-01 16:00

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1735823655',resources: [
+    cacheName: 'chirpy-1735844284',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
--- pt-BR/assets/js/data/swconf.js
+++ pt-BR/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1735823657',resources: [
+    cacheName: 'chirpy-1735844285',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
